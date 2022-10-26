ecc6c2b864c07250987a4e2681257411b0c69b47
sazib <sazibkhancse@gmail.com>
2022-10-26 12:15:09 +0600

    Update app

--- blogger-app/src/main/java/com/blogger/bloggerapp/BloggerAppApplication.java
+++ blogger-app/src/main/java/com/blogger/bloggerapp/BloggerAppApplication.java
@@ -8,7 +8,6 @@ public class BloggerAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BloggerAppApplication.class, args);
-		System.out.println("Successfull.....!");
 	}
 
 }
